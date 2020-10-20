d9a1cc5045bb68c64e648ce358a5b373b8183fe2
Tristan-Fullmer <ful15007@byui.edu>
2020-10-20 09:42:18 -0500

    Add labor.py

---
+++ work_order/models/labor.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+
+class MrpProductionWorkcenterLine(models.Model):
+    _inherit = 'mrp.workorder'
+    
+    @api.depends('qty_done', 'component_remaining_qty')
+    def _compute_component_qty_to_do(self):
+        for wo in self:
+            wo.component_qty_to_do = 12 % wo.component_remaining_qty
