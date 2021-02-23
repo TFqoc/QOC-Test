7a48241a6752dd96f490a8e429d0a5ddd97ba5d1
Tristan-Fullmer <ful15007@byui.edu>
2021-02-23 14:49:13 -0600

    Add version to sale order line

--- plm2/models/models.py
+++ plm2/models/models.py
@@ -1,18 +1,10 @@
 # -*- coding: utf-8 -*-
 
-# from odoo import models, fields, api
+from odoo import models, fields, api
 
 
-# class plm2(models.Model):
-#     _name = 'plm2.plm2'
-#     _description = 'plm2.plm2'
+class SaleLine(models.Model):
+    _inherit = 'sale.order.line'
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
+    version = fields.Integer()
+
