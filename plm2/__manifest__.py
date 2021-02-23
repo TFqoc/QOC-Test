be917cc3c36584f464a7cad00c30497f99938542
Tristan-Fullmer <ful15007@byui.edu>
2021-02-23 15:30:37 -0600

    Update __manifest__.py

--- plm2/__manifest__.py
+++ plm2/__manifest__.py
@@ -20,7 +20,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','sale','plm'],
+    'depends': ['base','sale','mrp_plm'],
 
     # always loaded
     'data': [
