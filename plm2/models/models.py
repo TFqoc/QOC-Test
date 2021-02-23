a4255bc2b2c7704e3b065c5c7c64c7f3cc578909
Tristan-Fullmer <ful15007@byui.edu>
2021-02-23 14:58:56 -0600

    Update models.py

--- plm2/models/models.py
+++ plm2/models/models.py
@@ -8,3 +8,16 @@ class SaleLine(models.Model):
 
     version = fields.Integer()
 
+    @api.model
+    def _prepare_add_missing_fields(self, values):
+        """ Deduce missing required fields from the onchange """
+        res = {}
+        onchange_fields = ['name', 'price_unit', 'product_uom', 'tax_id', 'version']
+        if values.get('order_id') and values.get('product_id') and any(f not in values for f in onchange_fields):
+            line = self.new(values)
+            line.product_id_change()
+            for field in onchange_fields:
+                if field not in values:
+                    res[field] = line._fields[field].convert_to_write(line[field], line)
+        return res
+
