6ca4598e48140bd81c1162121c288210f32f1f48
Tristan-Fullmer <ful15007@byui.edu>
2021-02-26 14:46:37 -0600

    Update __manifest__.py

--- plm2/__manifest__.py
+++ plm2/__manifest__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "qoc_plm_revision_management",
+    'name': "QOC PLM Revision Management",
 
     'summary': """
         Short (1 phrase/line) summary of the module's purpose, used as
