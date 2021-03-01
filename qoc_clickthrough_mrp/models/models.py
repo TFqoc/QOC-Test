66d86d92fa4387dd6e6383b554f9500ccc70b9f5
Tristan-Fullmer <ful15007@byui.edu>
2021-03-01 13:37:50 -0600

    Add tab on MO

--- qoc_clickthrough_mrp/models/models.py
+++ qoc_clickthrough_mrp/models/models.py
@@ -1,18 +1,14 @@
 # -*- coding: utf-8 -*-
 
-# from odoo import models, fields, api
+from odoo import models, fields, api
 
 
-# class qoc_clickthrough_mrp(models.Model):
-#     _name = 'qoc_clickthrough_mrp.qoc_clickthrough_mrp'
-#     _description = 'qoc_clickthrough_mrp.qoc_clickthrough_mrp'
+class qoc_clickthrough_mrp(models.Model):
+    _inherit = 'mrp.production'
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
+    #source_documents = fields.Many2many(comodel_name='')
+
+    # @api.depends('value')
+    # def _value_pc(self):
+    #     for record in self:
+    #         record.value2 = float(record.value) / 100
