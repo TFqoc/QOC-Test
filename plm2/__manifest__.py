4967a9d8bb6a463b81cf7d8eab01434134123a86
Tristan-Fullmer <ful15007@byui.edu>
2021-03-01 08:10:24 -0600

    Update __manifest__.py

--- plm2/__manifest__.py
+++ plm2/__manifest__.py
@@ -1,13 +1,16 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "QOC PLM Revision Management",
+    'name': "QOC - PLM Revision Management",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        Version Control Management""",
 
     'description': """
-        Long description of module's purpose
+        Take control of your version management! This app will create a new product variant
+        for each revision of a product you make. This way you can sell and produce both old
+        and new versions for each of your prouducts. Want to depricate and old version? All you 
+        have to do is remove the variant! All product information will be archived and no longer
+        available to sell. It's that easy!
     """,
 
     'author': "QOC Innovations",
@@ -16,7 +19,7 @@
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
+    'category': 'Manufacturing',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
