53296244fc9f402210914b6e3db36825afeca62b
Tristan-Fullmer <ful15007@byui.edu>
2021-03-02 10:38:41 -0600

    custom field

--- qoc_sales_order_delivery_schedule/models/models.py
+++ qoc_sales_order_delivery_schedule/models/models.py
@@ -8,14 +8,7 @@ class qoc_sales_order_delivery_schedule(models.Model):
     _description = 'Sales Order Delivery Schedule'
 
     name = fields.Char()
-    value = fields.Integer()
-    value2 = fields.Float(compute="_value_pc", store=True)
-    description = fields.Text()
-
-    @api.depends('value')
-    def _value_pc(self):
-        for record in self:
-            record.value2 = float(record.value) / 100
+    partner = fields.Many2one('res.partner')
 
 class ResPartner(models.Model):
     _inherit = 'res.partner'
