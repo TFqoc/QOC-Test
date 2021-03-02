379c2b7ed8dedcd3707144b417a51b64cc65b59b
Tristan-Fullmer <ful15007@byui.edu>
2021-03-02 15:55:26 -0600

    add monetary widget

---
+++ specific_decimals/js/monetary_decimals.js
@@ -0,0 +1,27 @@
+odoo.define('qoc.monetary_decimals', function (require) {
+    "use strict";
+
+    var monetary = require('web.basic_fields');
+    var fieldRegistry = require('web.field_registry');
+
+    var MonetaryDecimals = monetary.extend({
+        init: function () {
+            this._super.apply(this, arguments);
+    
+            this._setCurrency();
+    
+            if (this.mode === 'edit') {
+                this.tagName = 'div';
+                this.className += ' o_input';
+    
+                // do not display currency symbol in edit
+                this.formatOptions.noSymbol = true;
+            }
+    
+            this.formatOptions.currency = this.currency;
+            this.formatOptions.digits = [16, 6];
+            this.formatOptions.field_digits = this.nodeOptions.field_digits;
+        },
+    });
+    fieldRegistry.add('monetary_decimals', MonetaryDecimals);
+});
