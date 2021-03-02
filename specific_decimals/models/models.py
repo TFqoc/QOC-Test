cbf8d42cf0dfcd261433bc4640131f55b4d43a16
Tristan-Fullmer <ful15007@byui.edu>
2021-03-02 15:01:45 -0600

    Update models.py

--- specific_decimals/models/models.py
+++ specific_decimals/models/models.py
@@ -10,3 +10,10 @@ class specific_decimals(models.Model):
     price_subtotal = fields.Monetary(compute='_compute_amount', string='Subtotal', store=True, digits='Purchase Price')
     price_total = fields.Monetary(compute='_compute_amount', string='Total', store=True, digits='Purchase Price')
     price_tax = fields.Float(compute='_compute_amount', string='Tax', store=True, digits='Purchase Price')
+
+class purchase_decimals(models.Model):
+    _inherit = 'purchase.order'
+
+    amount_untaxed = fields.Monetary(string='Untaxed Amount', store=True, readonly=True, compute='_amount_all', tracking=True, digits='Purchase Price')
+    amount_tax = fields.Monetary(string='Taxes', store=True, readonly=True, compute='_amount_all', digits='Purchase Price')
+    amount_total = fields.Monetary(string='Total', store=True, readonly=True, compute='_amount_all', digits='Purchase Price')
