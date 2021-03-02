e5ff2326ce1f0219c5c6d07a1fd9362c30f71a45
Tristan-Fullmer <ful15007@byui.edu>
2021-03-02 14:50:58 -0600

    add more decimals

--- specific_decimals/models/models.py
+++ specific_decimals/models/models.py
@@ -6,3 +6,7 @@ class specific_decimals(models.Model):
     _inherit = 'purchase.order.line'
 
     price_unit = fields.Float(string='Unit Price', required=True, digits='Purchase Price')
+
+    price_subtotal = fields.Monetary(compute='_compute_amount', string='Subtotal', store=True, digits='Purchase Price')
+    price_total = fields.Monetary(compute='_compute_amount', string='Total', store=True, digits='Purchase Price')
+    price_tax = fields.Float(compute='_compute_amount', string='Tax', store=True, digits='Purchase Price')
