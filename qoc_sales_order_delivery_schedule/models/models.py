4e9a3c80faead413ee347af358751148e41de3cc
Tristan-Fullmer <ful15007@byui.edu>
2021-03-02 09:21:08 -0600

    Scaffold

---
+++ qoc_sales_order_delivery_schedule/models/models.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+
+class qoc_sales_order_delivery_schedule(models.Model):
+    _name = 'qoc_sales_order_delivery_schedule.qoc_sales_order_delivery_schedule'
+    _description = 'qoc_sales_order_delivery_schedule.qoc_sales_order_delivery_schedule'
+
+    name = fields.Char()
+    value = fields.Integer()
+    value2 = fields.Float(compute="_value_pc", store=True)
+    description = fields.Text()
+
+    @api.depends('value')
+    def _value_pc(self):
+        for record in self:
+            record.value2 = float(record.value) / 100
