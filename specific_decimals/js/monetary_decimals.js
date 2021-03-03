ec45bce572c37c3a5b0a419a3fa5e6b63db0d334
Tristan-Fullmer <ful15007@byui.edu>
2021-03-03 12:33:07 -0600

    Update monetary_decimals.js

--- specific_decimals/js/monetary_decimals.js
+++ specific_decimals/js/monetary_decimals.js
@@ -2,7 +2,43 @@ odoo.define('qoc.monetary_decimals', function (require) {
     "use strict";
 
     var monetary = require('web.basic_fields').FieldMonetary;
+    var field_utils = require('web.field_utils');
     var fieldRegistry = require('web.field_registry');
+
+    var my_utils = field_utils.include({
+        format2Monetary : function (value, field, options){
+        //function formatMonetary(value, field, options) {
+            if (value === false) {
+                return "";
+            }
+            options = Object.assign({ forceString: false }, options);
+        
+            var currency = options.currency;
+            if (!currency) {
+                var currency_id = options.currency_id;
+                if (!currency_id && options.data) {
+                    var currency_field = options.currency_field || field.currency_field || 'currency_id';
+                    currency_id = options.data[currency_field] && options.data[currency_field].res_id;
+                }
+                currency = session.get_currency(currency_id);
+            }
+        
+            var digits = 2
+            var formatted_value = formatFloat(value, field,
+                _.extend({}, options , {digits: digits})
+            );
+        
+            if (!currency || options.noSymbol) {
+                return formatted_value;
+            }
+            const ws = options.forceString ? ' ' : '&nbsp;';
+            if (currency.position === "after") {
+                return formatted_value + ws + currency.symbol;
+            } else {
+                return currency.symbol + ws + formatted_value;
+            }
+        }
+    });
 
     var MonetaryDecimals = monetary.extend({
         init: function () {
@@ -19,8 +55,8 @@ odoo.define('qoc.monetary_decimals', function (require) {
             }
     
             this.formatOptions.currency = this.currency;
-            this.formatOptions.digits = [16, 6];
-            this.formatOptions.field_digits = this.nodeOptions.field_digits;
+            this.formatOptions.digits = [16, 2];
+            this.formatOptions.field_digits = 2;
         },
     });
     fieldRegistry.add('monetary_decimals', MonetaryDecimals);
