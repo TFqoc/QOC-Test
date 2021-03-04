7912a4802322f6d63e0b04cb2bc1ed22f0867ca9
Tristan-Fullmer <tfullmer@qocinnovations.com>
2021-03-04 13:26:11 -0600

    new custom view

--- qoc_sales_order_delivery_schedule/models/models.py
+++ qoc_sales_order_delivery_schedule/models/models.py
@@ -15,3 +15,13 @@ class ResPartner(models.Model):
 
     def my_method(self):
         pass
+
+class View(models.Model):
+    _inherit = 'ir.ui.view'
+
+    type = fields.Selection(selection_add=[('delivery_schedule','"Delivery Schedule')])
+
+class ActWindowView(models.Model):
+    _inherit = 'ir.actions.act_window.view'
+
+    view_mode = fields.Selection(selection_add=[('delivery_schedule','"Delivery Schedule')])
