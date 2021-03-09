e5b96f619ed26e2866c9272f2ec43db6284f99a7
Tristan-Fullmer <tfullmer@qocinnovations.com>
2021-03-09 14:41:01 -0600

    Controller test

--- qoc_sales_order_delivery_schedule/controllers/controllers.py
+++ qoc_sales_order_delivery_schedule/controllers/controllers.py
@@ -1,21 +1,25 @@
 # -*- coding: utf-8 -*-
-# from odoo import http
+from odoo import http
 
 
-# class QocSalesOrderDeliverySchedule(http.Controller):
-#     @http.route('/qoc_sales_order_delivery_schedule/qoc_sales_order_delivery_schedule/', auth='public')
-#     def index(self, **kw):
-#         return "Hello, world"
+class DeliveryScheduleManager(http.Controller):
+    @http.route('/qoc_sales_order_delivery_schedule/qoc_sales_order_delivery_schedule/', auth='public')
+    def index(self, **kw):
+        return "Hello, world"
 
-#     @http.route('/qoc_sales_order_delivery_schedule/qoc_sales_order_delivery_schedule/objects/', auth='public')
-#     def list(self, **kw):
-#         return http.request.render('qoc_sales_order_delivery_schedule.listing', {
-#             'root': '/qoc_sales_order_delivery_schedule/qoc_sales_order_delivery_schedule',
-#             'objects': http.request.env['qoc_sales_order_delivery_schedule.qoc_sales_order_delivery_schedule'].search([]),
-#         })
+    @http.route('/qoc_sales_order_delivery_schedule/qoc_sales_order_delivery_schedule/objects/', auth='public')
+    def list(self, **kw):
+        return http.request.render('qoc_sales_order_delivery_schedule.listing', {
+            'root': '/qoc_sales_order_delivery_schedule/qoc_sales_order_delivery_schedule',
+            'objects': http.request.env['qoc_sales_order_delivery_schedule.qoc_sales_order_delivery_schedule'].search([]),
+        })
 
-#     @http.route('/qoc_sales_order_delivery_schedule/qoc_sales_order_delivery_schedule/objects/<model("qoc_sales_order_delivery_schedule.qoc_sales_order_delivery_schedule"):obj>/', auth='public')
-#     def object(self, obj, **kw):
-#         return http.request.render('qoc_sales_order_delivery_schedule.object', {
-#             'object': obj
-#         })
+    @http.route('/qoc_sales_order_delivery_schedule/qoc_sales_order_delivery_schedule/objects/<model("qoc_sales_order_delivery_schedule.qoc_sales_order_delivery_schedule"):obj>/', auth='public')
+    def object(self, obj, **kw):
+        return http.request.render('qoc_sales_order_delivery_schedule.object', {
+            'object': obj
+        })
+
+    @http.route('/ds/mo_data/', auth='public')
+    def mo_data(self, **kw):
+        return http.request.env['mrp.production'].search([])
