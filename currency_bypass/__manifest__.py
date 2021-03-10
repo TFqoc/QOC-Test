0b02373aa1eea965ab80857d97456523029a8289
Tristan-Fullmer <tfullmer@qocinnovations.com>
2021-03-10 13:31:25 -0600

    clean up files

--- currency_bypass/__manifest__.py
+++ currency_bypass/__manifest__.py
@@ -6,7 +6,7 @@
         Allows you to bypass the error when reducing decimal precision on a currency.""",
 
     'description': """
-        Long description of module's purpose
+        Allows you to bypass the error when reducing decimal precision on a currency.
     """,
 
     'author': "QOC Innovations",
@@ -16,19 +16,17 @@
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Uncategorized',
-    'version': '0.1',
+    'version': '1',
 
     # any module necessary for this one to work correctly
     'depends': ['base'],
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
+
     ],
     # only loaded in demonstration mode
     'demo': [
-        'demo/demo.xml',
+
     ],
 }
