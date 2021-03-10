757640a9e93a54f46176916bad4463af2fa9f446
Tristan-Fullmer <tfullmer@qocinnovations.com>
2021-03-10 13:38:59 -0600

    add comments

--- currency_bypass/models/models.py
+++ currency_bypass/models/models.py
@@ -6,5 +6,7 @@ from odoo import models, fields, api
 class currency_bypass(models.Model):
     _inherit = 'res.currency'
 
+    # Skip the error instructions defined in account>models>res_currency.py
+    # Just call the default write function instead
     def write(self, vals):
         super(models.Model, self).write(vals)
