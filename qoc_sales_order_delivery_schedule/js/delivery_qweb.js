cf94754f156c10fcc2940b00f78e7563a2c5832b
Tristan-Fullmer <tfullmer@qocinnovations.com>
2021-03-11 11:17:30 -0600

    update js

--- qoc_sales_order_delivery_schedule/js/delivery_qweb.js
+++ qoc_sales_order_delivery_schedule/js/delivery_qweb.js
@@ -11,26 +11,51 @@ function toggle_dropdown(el){
 }
 function get_mo_data(el){
   id = el.getAttribute("model_id");
+  next_box = String(next_box_id(el));
+  current_box = get_box_id(el);
+
   $.ajax({
       url: "/qoc_sales_order_delivery_schedule/qoc_sales_order_delivery_schedule/mo_data/"+String(id),
       success: function( result ) {
-        $( "#box_2" ).html(result);
+        if (next_box % 2 == 1 && $('#box_'+(current_box+2)).length == 0){
+          // Create new row
+          id1 = current_box+2;
+          id2 = id1+1;
+          $("#page_container").append('<div class="row justify-content-around mb-4"><div class="col"><div class="ds_card" id="box_'+id1+'"><span>Content</span></div></div><div class="col"><div class="ds_card" id="box_'+id2+'"><span>Content</span></div></div></div>');
+        }
+        $( "#box_"+next_box).html(result);
       }
     });
-  select(el,1);
+    console.log("Next: "+next_box+"\nCurrent: "+current_box);
+  select(el,current_box);
 }
 function get_wo_data(el){
   id = el.getAttribute("model_id");
+  next_box = String(next_box_id(el));
+  current_box = get_box_id(el);
+
   $.ajax({
     url: "/qoc_sales_order_delivery_schedule/qoc_sales_order_delivery_schedule/wo_data/"+String(id),
     success: function( result ) {
-      $( "#box_3" ).html(result);
+      if (next_box % 2 == 1 && $('#box_'+(current_box+2)).length == 0){
+        // Create new row
+        id1 = current_box+2;
+        id2 = id1+1;
+        $("#page_container").append('<div class="row justify-content-around mb-4"><div class="col"><div class="ds_card" id="box_'+id1+'"><span>Content</span></div></div><div class="col"><div class="ds_card" id="box_'+id2+'"><span>Content</span></div></div></div>');
+      }
+      $( "#box_"+next_box).html(result);
     }
   });
-  select(el,2);
+  console.log("Next: "+next_box+"\nCurrent: "+current_box);
+  select(el,current_box);
 }
 function select(el, box){
   $("#box_"+String(box)).find(".selected").removeClass("selected");
   el.classList.add("selected");
 }
-//
+function get_box_id(el){
+  return $(el).closest(".ds_card").attr('id').substring(4);
+}
+function next_box_id(el){
+  return eval($(el).closest(".ds_card").attr('id').substring(4)+" + 1");
+}
