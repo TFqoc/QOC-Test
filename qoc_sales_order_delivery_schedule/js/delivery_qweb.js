04b9456bb7ee8dd6173930376626c7f34e9fd604
Tristan-Fullmer <tfullmer@qocinnovations.com>
2021-03-12 12:20:28 -0600

    Update delivery_qweb.js

--- qoc_sales_order_delivery_schedule/js/delivery_qweb.js
+++ qoc_sales_order_delivery_schedule/js/delivery_qweb.js
@@ -9,44 +9,24 @@ function toggle_dropdown(el){
     el.className = 'fa fa-caret-down dropdown-content';
   }
 }
-function get_mo_data(el){
+function get_data(el,path){
   id = el.getAttribute("model_id");
   next_box = String(next_box_id(el));
   current_box = get_box_id(el);
 
   $.ajax({
-      url: "/qoc_sales_order_delivery_schedule/qoc_sales_order_delivery_schedule/mo_data/"+String(id),
+      url: "/qoc_sales_order_delivery_schedule/qoc_sales_order_delivery_schedule/"+path+"/"+String(id),
       success: function( result ) {
-        if (next_box % 2 == 1 && $('#box_'+(current_box+2)).length == 0){
+        id1 = parseInt(current_box) + 1;
+        id2 = id1 + 1;
+        if (next_box % 2 == 1 && $('#box_'+id1).length == 0){
           // Create new row
-          id1 = current_box+2;
-          id2 = id1+1;
           $("#page_container").append('<div class="row justify-content-around mb-4"><div class="col"><div class="ds_card" id="box_'+id1+'"><span>Content</span></div></div><div class="col"><div class="ds_card" id="box_'+id2+'"><span>Content</span></div></div></div>');
         }
-        $( "#box_"+next_box).html(result);
+        $("#box_" + next_box).html(result);
+        $("#box_" + next_box).find(".dropdown-content:not(.no-hide)").hide();
       }
     });
-    console.log("Next: "+next_box+"\nCurrent: "+current_box);
-  select(el,current_box);
-}
-function get_wo_data(el){
-  id = el.getAttribute("model_id");
-  next_box = String(next_box_id(el));
-  current_box = get_box_id(el);
-
-  $.ajax({
-    url: "/qoc_sales_order_delivery_schedule/qoc_sales_order_delivery_schedule/wo_data/"+String(id),
-    success: function( result ) {
-      if (next_box % 2 == 1 && $('#box_'+(current_box+2)).length == 0){
-        // Create new row
-        id1 = current_box+2;
-        id2 = id1+1;
-        $("#page_container").append('<div class="row justify-content-around mb-4"><div class="col"><div class="ds_card" id="box_'+id1+'"><span>Content</span></div></div><div class="col"><div class="ds_card" id="box_'+id2+'"><span>Content</span></div></div></div>');
-      }
-      $( "#box_"+next_box).html(result);
-    }
-  });
-  console.log("Next: "+next_box+"\nCurrent: "+current_box);
   select(el,current_box);
 }
 function select(el, box){
