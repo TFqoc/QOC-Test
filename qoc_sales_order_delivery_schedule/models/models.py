856d86331cff551c87185c80bad17e4673cf6948
Tristan-Fullmer <tfullmer@qocinnovations.com>
2021-03-12 14:32:09 -0600

    Update models.py

--- qoc_sales_order_delivery_schedule/models/models.py
+++ qoc_sales_order_delivery_schedule/models/models.py
@@ -50,8 +50,8 @@ class WorkOrder(models.Model):
         products = []
         for p in self.check_ids:
             products.append(p.product_id)
-        _logger.info("LENGTH: " + str(len(products)))
+        _logger.info("\nLENGTH: " + str(len(products)))
         for line in self.production_id.move_raw_ids:
-            # if line.product_id in products:
-            res.append(line)
+            if line.product_id in products:
+                res.append(line)
         return res
