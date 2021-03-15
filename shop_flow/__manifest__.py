0dcfee5c2a481c001dd8eebc635ca76d7de0116f
Tristan-Fullmer <tfullmer@qocinnovations.com>
2021-03-15 08:36:24 -0500

    Name change to ShopFlow

--- shop_flow/__manifest__.py
+++ shop_flow/__manifest__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "qoc_sales_order_delivery_schedule",
+    'name': "Shop Flow",
 
     'summary': """
         Short (1 phrase/line) summary of the module's purpose, used as
