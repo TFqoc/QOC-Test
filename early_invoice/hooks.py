45fb13cf7bf53716c2d69315520a96e6fcd4c0fd
Tristan-Fullmer <tfullmer@qocinnovations.com>
2021-03-19 09:47:55 -0500

    Update hooks.py

--- early_invoice/hooks.py
+++ early_invoice/hooks.py
@@ -3,9 +3,9 @@ import logging
 
 _logger = logging.getLogger(__name__)
 
-# def post_init_hook(cr, registry):
-#     env = api.Environment(cr, SUPERUSER_ID, {})
-#     records = env['sale.order'].search([])
-#     records._compute_delivered()
-#     records._has_invoice_compute()
-#     _logger.info("\nHOOK RAN")
+def post_init_hook(cr, registry):
+    env = api.Environment(cr, SUPERUSER_ID, {})
+    records = env['sale.order'].search([])
+    records._compute_delivered()
+    records._has_invoice_compute()
+    _logger.info("\nHOOK RAN")
