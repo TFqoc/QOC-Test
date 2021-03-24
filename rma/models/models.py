01c58d1ac553e8f7e114a2ca9c2ac4db8d15ef04
Tristan-Fullmer <tfullmer@qocinnovations.com>
2021-03-24 15:43:13 -0500

    Add debug logs

--- rma/models/models.py
+++ rma/models/models.py
@@ -2,6 +2,8 @@
 from collections import defaultdict
 from random import randint
 import datetime
+import logging
+_logger = logging.getLogger(__name__)
 
 from odoo import models, fields, api, _
 from odoo.exceptions import UserError, ValidationError
@@ -502,3 +504,50 @@ class StockMove(models.Model):
     _inherit = 'stock.move'
 
     forecast_availability = fields.Float('Forcast Availability', compute='_compute_forecast_information', digits='Product Unit of Measure', store=True)
+
+
+class MrpImmediateProduction(models.TransientModel):
+    _inherit = 'mrp.immediate.production'
+
+    def process(self):
+        _logger.info("\nProcess 1")
+        productions_to_do = self.env['mrp.production']
+        productions_not_to_do = self.env['mrp.production']
+        for line in self.immediate_production_line_ids:
+            if line.to_immediate is True:
+                productions_to_do |= line.production_id
+            else:
+                productions_not_to_do |= line.production_id
+
+        _logger.info("\nProcess 2")
+        for production in productions_to_do:
+            error_msg = ""
+            if production.product_tracking in ('lot', 'serial') and not production.lot_producing_id:
+                production.action_generate_serial()
+            if production.product_tracking == 'serial' and float_compare(production.qty_producing, 1, precision_rounding=production.product_uom_id.rounding) == 1:
+                production.qty_producing = 1
+            else:
+                production.qty_producing = production.product_qty - production.qty_produced
+            production._set_qty_producing()
+            for move in production.move_raw_ids.filtered(lambda m: m.state not in ['done', 'cancel']):
+                rounding = move.product_uom.rounding
+                for move_line in move.move_line_ids:
+                    if move_line.product_uom_qty:
+                        move_line.qty_done = min(move_line.product_uom_qty, move_line.move_id.should_consume_qty)
+                    if float_compare(move.quantity_done, move.should_consume_qty, precision_rounding=rounding) >= 0:
+                        break
+                if float_compare(move.product_uom_qty, move.quantity_done, precision_rounding=move.product_uom.rounding) == 1:
+                    if move.has_tracking in ('serial', 'lot'):
+                        error_msg += "\n  - %s" % move.product_id.display_name
+
+            if error_msg:
+                error_msg = _('You need to supply Lot/Serial Number for products:') + error_msg
+                raise UserError(error_msg)
+        _logger.info("\nProcess 3")
+        productions_to_validate = self.env.context.get('button_mark_done_production_ids')
+        if productions_to_validate:
+            productions_to_validate = self.env['mrp.production'].browse(productions_to_validate)
+            productions_to_validate = productions_to_validate - productions_not_to_do
+            return productions_to_validate.with_context(skip_immediate=True).button_mark_done()
+        _logger.info("\nProcess 4")
+        return True
