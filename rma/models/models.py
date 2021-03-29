265bf1ae645576f6232b66079ea7c3a37c8b48aa
Tristan-Fullmer <tfullmer@qocinnovations.com>
2021-03-29 09:15:55 -0500

    Update models.py

--- rma/models/models.py
+++ rma/models/models.py
@@ -539,7 +539,7 @@ class RMA(models.Model):
 
         repairs.write({'invoiced': True})
         repairs.mapped('operations').filtered(lambda op: op.type == 'add').write({'invoiced': True})
-        repairs.mapped('fees_lines').write({'invoiced': True})
+        # repairs.mapped('fees_lines').write({'invoiced': True})
 
         return dict((repair.id, repair.invoice_id.id) for repair in repairs)
 
@@ -741,3 +741,19 @@ class StockWarnInsufficientQtyRepair(models.TransientModel):
     def action_done(self):
         self.ensure_one()
         return self.repair_id.action_repair_confirm()
+
+class AccountMove(models.Model):
+    _inherit = 'account.move'
+
+    repair_ids = fields.One2many('rma.rma', 'invoice_id', readonly=True, copy=False)
+
+    def unlink(self):
+        self.repair_ids.filtered(lambda repair: repair.state != 'cancel').state = '2binvoiced'
+        return super().unlink()
+
+
+class AccountMoveLine(models.Model):
+    _inherit = 'account.move.line'
+
+    repair_line_ids = fields.One2many('repair.line', 'invoice_line_id', readonly=True, copy=False)
+    # repair_fee_ids = fields.One2many('repair.fee', 'invoice_line_id', readonly=True, copy=False)
