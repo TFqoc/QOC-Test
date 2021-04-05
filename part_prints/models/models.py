3abfd2072b1bde487117c6c0e52269a580dd3fb0
Tristan-Fullmer <tfullmer@qocinnovations.com>
2021-04-05 13:37:12 -0500

    Attachments on rfq

--- part_prints/models/models.py
+++ part_prints/models/models.py
@@ -22,3 +22,32 @@ class MRP(models.Model):
     _inherit = 'mrp.production'
     
     part_print = fields.Binary(related='bom_id.part_print')
+
+class PurchaseOrder(models.Model):
+    _inherit = 'purchase.order'
+
+    def action_rfq_send(self):
+        # Do some cool stuff to add all the attachments to the email
+        # purchase.py line 305
+        # Check the template used and see if you can edit that to put attachements there.
+        pass
+
+class MailComposer(models.TransientModel):
+    _inherit = 'mail.compose.message'
+
+    @api.model
+    def create(self, vals):
+        if self.env.context['active_model'] == 'purchase.order':
+            order = self.env.browse(self.env.context['active_id'])
+            ids = []
+            for line in order.order_line:
+                if line.product_id.part_print:
+                    ids.append(self.env['ir.attachment'].create({
+                        'name':'Part Print',
+                        'type':'binary',
+                        'db_datas':line.product_id.part_print,
+                    }).id)
+            vals.update({
+                'attachment_ids': [(6,0,ids)],
+            })
+        return super(MailComposer, self).create(vals)
