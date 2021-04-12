071c8b5b7422f8dda4adeb43493b375f451444df
Tristan-Fullmer <tfullmer@qocinnovations.com>
2021-04-12 11:35:11 -0500

    Update sql_db.py

--- ps-runbot-testing-13.0/runbot_testing_recording/controllers/sql_db.py
+++ ps-runbot-testing-13.0/runbot_testing_recording/controllers/sql_db.py
@@ -8,7 +8,7 @@ old_execute = Cursor.execute
 def new_execute(self, query, params=None, log_exceptions=None):
     result = old_execute(self, query, params, log_exceptions)
 
-    string_query = query if isinstance(query,str) else query.string
+    string_query = query if isinstance(query,str) else query.as_string()
 
     if re_write_db.search(string_query):
         self.method_is_writing_in_db = True
