d679e207fdbba8a5c17d081166072d6eae76c2a2
JMC-ai <80903570+JMC-ai@users.noreply.github.com>
2021-04-12 14:17:09 -0700

    Update sql_db.py

--- ps-runbot-testing-13.0/runbot_testing_recording/controllers/sql_db.py
+++ ps-runbot-testing-13.0/runbot_testing_recording/controllers/sql_db.py
@@ -12,7 +12,7 @@ def new_execute(self, query, params=None, log_exceptions=None):
     result = old_execute(self, query, params, log_exceptions)
 
     #Prints the type of the query as a debug message.
-    _logger.debug("QUERY IS A", type(query))
+    # _logger.debug("QUERY IS A", type(query))
 
     string_query = query if isinstance(query,str) else query.as_string(Cursor)
 
